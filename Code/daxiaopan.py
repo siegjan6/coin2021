169f945ece1ac11ec4a07585a3d98154f0dacf98
siegjan6 <steven.jan@foxmail.com>
2021-05-06 10:04:26 +0800

    path
    path

--- Code/daxiaopan.py
+++ Code/daxiaopan.py
@@ -2,6 +2,9 @@ import ccxt
 from datetime import timedelta
 import pandas as pd
 import time
+import sys,os
+if sys.platform != 'win32':
+    sys.path.append('/root/coin2021')
 from base.Tool import *
 from Code.base import wechat
 
